b23e9f6ed98cde9959d0189f8572066ba8b7a909
Shubham05116 <shubham05116@gmail.com>
2023-10-01 18:17:53 +0530

    Initialize project using Create React App

---
+++ data.js
@@ -0,0 +1,159 @@
+export default data = [
+        {
+            "id": 1,
+            "image": "car_image_url_1",
+            "name": "Car Name 1",
+            "year_launched": 2020,
+            "seating_capacity": 5,
+            "fuel_type": "Gasoline",
+            "mileage":8,
+            "transmission": "Automatic",
+            "price": 200,
+                
+            
+        },
+        {
+            "id": 2,
+            "image": "car_image_url_1",
+            "name": "Car Name 1",
+            "year_launched": 2020,
+            "seating_capacity": 5,
+            "fuel_type": "Gasoline",
+            "mileage":8,
+            "transmission": "Automatic",
+            "price": 200,
+                
+            
+        },
+        {
+            "id": 3,
+            "image": "car_image_url_1",
+            "name": "Car Name 1",
+            "year_launched": 2020,
+            "seating_capacity": 5,
+            "fuel_type": "Gasoline",
+            "mileage":8,
+            "transmission": "Automatic",
+            "price": 200,
+                
+            
+        },
+        {
+            "id": 4,
+            "image": "car_image_url_1",
+            "name": "Car Name 1",
+            "year_launched": 2020,
+            "seating_capacity": 5,
+            "fuel_type": "Gasoline",
+            "mileage":8,
+            "transmission": "Automatic",
+            "price": 200,
+                
+            
+        },
+        {
+            "id": 5,
+            "image": "car_image_url_1",
+            "name": "Car Name 1",
+            "year_launched": 2020,
+            "seating_capacity": 5,
+            "fuel_type": "Gasoline",
+            "mileage":8,
+            "transmission": "Automatic",
+            "price": 200,
+                
+            
+        },
+        {
+            "id": 6,
+            "image": "car_image_url_1",
+            "name": "Car Name 1",
+            "year_launched": 2020,
+            "seating_capacity": 5,
+            "fuel_type": "Gasoline",
+            "mileage":8,
+            "transmission": "Automatic",
+            "price": 200,
+                
+            
+        },
+        {
+            "id": 7,
+            "image": "car_image_url_1",
+            "name": "Car Name 1",
+            "year_launched": 2020,
+            "seating_capacity": 5,
+            "fuel_type": "Gasoline",
+            "mileage":8,
+            "transmission": "Automatic",
+            "price": 200,
+                
+            
+        },
+        {
+            "id": 8,
+            "image": "car_image_url_1",
+            "name": "Car Name 1",
+            "year_launched": 2020,
+            "seating_capacity": 5,
+            "fuel_type": "Gasoline",
+            "mileage":8,
+            "transmission": "Automatic",
+            "price": 200,
+                
+            
+        },
+        {
+            "id": 9,
+            "image": "car_image_url_1",
+            "name": "Car Name 1",
+            "year_launched": 2020,
+            "seating_capacity": 5,
+            "fuel_type": "Gasoline",
+            "mileage":8,
+            "transmission": "Automatic",
+            "price": 200,
+                
+            
+        },
+        {
+            "id": 10,
+            "image": "car_image_url_1",
+            "name": "Car Name 1",
+            "year_launched": 2020,
+            "seating_capacity": 5,
+            "fuel_type": "Gasoline",
+            "mileage":8,
+            "transmission": "Automatic",
+            "price": 200,
+                
+            
+        },
+        {
+            "id": 11,
+            "image": "car_image_url_1",
+            "name": "Car Name 1",
+            "year_launched": 2020,
+            "seating_capacity": 5,
+            "fuel_type": "Gasoline",
+            "mileage":8,
+            "transmission": "Automatic",
+            "price": 200,
+                
+            
+        },
+        {
+            "id": 12,
+            "image": "car_image_url_1",
+            "name": "Car Name 1",
+            "year_launched": 2020,
+            "seating_capacity": 5,
+            "fuel_type": "Gasoline",
+            "mileage":8,
+            "transmission": "Automatic",
+            "price": 200,
+                
+            
+        },
+    ]
+}
